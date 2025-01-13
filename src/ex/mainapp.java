6e0cfa5218b44fd5780bf59485b36cca70cd03d7
IrasogAlex <irasog.alexandru@gmail.com>
2025-01-13 08:48:43 +0200

    Tema2 primul commit

---
+++ src/ex/mainapp.java
@@ -0,0 +1,13 @@
+package ex;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class mainapp {
+    public static void main(String[] args) {
+
+        String s= "abc" ;
+        s.toUpperCase(); System.out.println(s);
+    }
+}
